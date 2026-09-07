862509b0309ea2aed099032af65a0ce80a0aa19f
AlanacondaX09 <alisamirmostafa09@gmail.com>
2026-09-07 17:51:32 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,21 @@
-const CACHE_NAME = "grammar-v2";
+const CACHE_NAME = "grammar-v1";
 
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
-    "./Lessons.html",
-    "./Exercises.html",
-    "./Results.html",
-    "./Settings.html",
     "./Style.css",
     "./script.js",
-    "./lessons.js",
-    "./exercises.js",
-    "./results.js",
     "./manifest.json",
-    "./icon/icon-192.png",
-    "./icon/icon-512.png"
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then((cache) => {
+                return cache.addAll(FILES_TO_CACHE);
+            })
     );
 
     self.skipWaiting();
@@ -28,13 +23,13 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("activate", (event) => {
     event.waitUntil(
-        caches.keys().then((cacheNames) =>
-            Promise.all(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
                 cacheNames
                     .filter((name) => name !== CACHE_NAME)
                     .map((name) => caches.delete(name))
-            )
-        )
+            );
+        })
     );
 
     self.clients.claim();
