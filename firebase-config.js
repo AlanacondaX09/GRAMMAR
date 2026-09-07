75cf8427f368f817a60bf06397f944a16ef4ac9a
AlanacondaX09 <alisamirmostafa09@gmail.com>
2026-09-07 16:59:22 +0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDqQEhhCErJ0U2mDxY1oYQIhTSwC9QCeqc",
-  authDomain: "grammar-app-2026.firebaseapp.com",
-  projectId: "grammar-app-2026",
-  storageBucket: "grammar-app-2026.firebasestorage.app",
-  messagingSenderId: "207637980729",
-  appId: "1:207637980729:web:a993368ad9fb67f4228fe8"
-};
